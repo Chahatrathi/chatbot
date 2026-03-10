965e4a4cc043837cf0ddabb80e8bc591b3b47df4
Chahatrathi <158983843+Chahatrathi@users.noreply.github.com>
2026-03-10 15:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pypdf import PdfReader
 from dotenv import load_dotenv
 
 # New Imports for RAG
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
